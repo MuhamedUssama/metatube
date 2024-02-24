94326b4f2d6ade8792531c079f23d1b59e639a94
Mohamed <mohamed.osama200538@gmail.com>
2024-02-24 21:06:58 +0200

    Handling Saving Our Content

--- lib/services/file_services.dart
+++ lib/services/file_services.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:metatube/utils/snakbar_utils.dart';
 
 class FileService {
   final TextEditingController textController = TextEditingController();
@@ -9,6 +10,23 @@ class FileService {
 
   bool fieldsNotEmpty = false;
 
-  File? _selectedField;
+  File? _selectedFile;
   String _selectedDirectory = "";
+
+  void saveContent(context) async {
+    final title = textController.text;
+    final discription = descriptionController.text;
+    final tags = tagsController.text;
+
+    final textContent =
+        "Title:\n\t$title\n\nDiscription:\n\t$discription\n\nTags:\n\t$tags\n";
+
+    try {
+      if (_selectedFile != null) {
+        await _selectedFile!.writeAsString(textContent);
+      }
+    } catch (e) {
+      SnakBarUtils.showSnakbar(context, Icons.error_outline, "File Not Saved");
+    }
+  }
 }
