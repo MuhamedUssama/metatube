e0e3620998eb5a17091828ca7216f01b5da3d367
Mohamed <mohamed.osama200538@gmail.com>
2024-02-26 12:33:31 +0200

    Finished splash screen

--- lib/screens/spalsh_screen.dart
+++ lib/screens/spalsh_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:metatube/screens/home_screen.dart';
 import 'package:metatube/utils/app_style.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static const String routeName = "splash screen";
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(milliseconds: 1500), () {
+      Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
